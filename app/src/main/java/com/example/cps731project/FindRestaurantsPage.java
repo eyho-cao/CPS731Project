0b1c888bb5733b937d0e31ba89d4010cfa4a62f3
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2020-11-22 15:26:36 -0500

    Implemented search results
    Takes user selection inputs and gets results of search.
    Parses search results to match star criteria and outputs list of ratings, restaurant names, and addresses

--- app/src/main/java/com/example/cps731project/FindRestaurantsPage.java
+++ app/src/main/java/com/example/cps731project/FindRestaurantsPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -12,34 +13,51 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class FindRestaurantsPage extends AppCompatActivity{
 
     protected LocationManager locationManager;
     protected LocationListener listener;
-    protected Context context;
-    String lat;
-    String provider;
     protected String latitude, longitude;
+
+    Spinner star;
+    Spinner type;
+    Spinner price;
     TextView txtV;
     PlacesClient placesClient;
     ApplicationInfo app;
     Bundle bundle;
     Button setLocation;
     Button findRestaurants;
+    Switch open;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +67,10 @@ public class FindRestaurantsPage extends AppCompatActivity{
         txtV = findViewById(R.id.textView2);
         setLocation = findViewById(R.id.setLocationBtn);
         findRestaurants = findViewById(R.id.findRestaurantsBtn);
+        star = findViewById(R.id.starsSpinner);
+        type = findViewById(R.id.typeSpinner);
+        price = findViewById(R.id.priceSpinner);
+        open = findViewById(R.id.openSwitch);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         listener = new LocationListener() {
             @Override
@@ -153,6 +175,120 @@ public class FindRestaurantsPage extends AppCompatActivity{
 
     private void FindRestaurants()
     {
-        
+
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latitude +", " +longitude +"&radius=2000&type=restaurant";
+        //String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.65822542336006, -79.38159876389952&radius=2000&type=restaurant";
+        String restaurantType = type.getSelectedItem().toString();
+        if(!restaurantType.equals("N/A"))
+        {
+            url+="&keyword=" +restaurantType;
+        }
+        String priceRange = price.getSelectedItem().toString();
+        url+="&minprice="+priceRange;
+        Boolean openNow = open.isChecked();
+        if(openNow)
+        {
+            url+="&opennow";
+        }
+        url+="&key="+bundle.getString("com.google.android.geo.API_KEY");
+        Log.d("QuerySent", url);
+        new JsonTask().execute(url);
+    }
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                ParseResults(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    JSONObject reader;
+    ArrayList<Double> ratings= new ArrayList<>();
+    ArrayList<String> restaurants = new ArrayList<>();
+    ArrayList<String> addresses = new ArrayList<>();
+    void ParseResults(String jsonString) throws JSONException {
+        int minStar;
+        if(star.getSelectedItem().toString() != "N/A")
+        {
+            minStar = 1;
+        }
+        else
+        {
+            minStar = Integer.parseInt(star.getSelectedItem().toString());
+        }
+        reader = new JSONObject(jsonString);
+        JSONArray results = reader.getJSONArray("results");
+        for(int i = 0; i < results.length(); i++)
+        {
+            JSONObject temp = results.getJSONObject(i);
+            double tempRating = temp.getDouble("rating");
+            if(tempRating >= minStar)
+            {
+                ratings.add(tempRating);
+                restaurants.add(temp.getString("name"));
+                addresses.add(temp.getString("vicinity"));
+            }
+        }
+        LaunchRestaurantsList();
+    }
+
+    void LaunchRestaurantsList()
+    {
+        Intent intent = new Intent(this, RestaurantsListPage.class);
+        intent.putExtra("restaurants", restaurants);
+        intent.putExtra("ratings", ratings);
+        intent.putExtra("address", addresses);
+        startActivity(intent);
     }
 }
